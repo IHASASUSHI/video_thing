46f8dacd3c6f94491661aa7b28aa34d64da3dd3e
IHASASUSHI <raathka@gmail.com>
2021-12-20 12:42:38 -0500

    changed to V4L2_PIX_FMT_YUYV

--- video.c
+++ video.c
@@ -240,7 +240,7 @@ static void init_device(void) {
     CLEAR(fmt);
     fmt.fmt.pix.width = 3280;
     fmt.fmt.pix.height = 2464;
-    fmt.fmt.pix.pixelformat = V4L2_PIX_FMT_JPEG;
+    fmt.fmt.pix.pixelformat = V4L2_PIX_FMT_YUYV;
     fmt.fmt.pix.field = V4L2_FIELD_INTERLACED;
 
     if (-1 == xioctl(fd, VIDIOC_S_FMT, &fmt))
