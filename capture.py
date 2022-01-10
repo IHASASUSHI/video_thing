d79156333c31f9e0db20daa0b0fee713254c990e
IHASASUSHI <raathka@gmail.com>
2022-01-10 21:31:26 +0000

    updates

--- capture.py
+++ capture.py
@@ -1,4 +1,5 @@
 from ctypes import *
+from sys import getsizeof
 import os
 
 class timeval(Structure):
@@ -88,5 +89,6 @@ if __name__ == "__main__":
         _libvideo.open_device(device)
         print("get frame")
         frame = _libvideo.get_frame_user_ptr(device)
+        print(getsizeof(frame.m.userptr))
         file.write([i.to_bytes(4, 'big') for i in cast(frame.m.userptr, POINTER(c_ulong))[:frame.bytesused//4]])
         _libvideo.open_device(device)
