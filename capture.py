4907da2b01a9ebf486560b1093fa48bcf32d3aa9
IHASASUSHI <raathka@gmail.com>
2022-01-10 16:32:18 -0500

    update

--- capture.py
+++ capture.py
@@ -89,6 +89,6 @@ if __name__ == "__main__":
         _libvideo.open_device(device)
         print("get frame")
         frame = _libvideo.get_frame_user_ptr(device)
-        print(getsizeof(frame.m.userptr))
+        print(getsizeof(cast(frame.m.userptr, POINTER(c_ulong))[0]))
         file.write([i.to_bytes(4, 'big') for i in cast(frame.m.userptr, POINTER(c_ulong))[:frame.bytesused//4]])
         _libvideo.open_device(device)
